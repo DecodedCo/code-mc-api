1c45f80e3d4b669518d38b443373c69ffb3febfd
jeffreylancaster <jeffrey@decoded.com>
2017-10-17 11:02:48 -0400

    Add script.js and script-no-jquery.js

---
+++ script-no-jquery.js
@@ -0,0 +1,78 @@
+// Does not require jQuery
+var request = new XMLHttpRequest();
+request.open('GET', '', true);
+
+request.onload = function() {
+  if (this.status >= 200 && this.status < 400) {
+    // Success!
+    var data = JSON.parse(this.response);
+    var keys = Object.keys(data.rates);
+    document.getElementById("bottom").innerHTML = "1 " + data.base + " = " + data.rates[keys[0]].toFixed(2) + " " + keys[0];
+  } else {
+    console.log("server returned an error");
+  }
+};
+
+request.onerror = function() {
+  console.log("connection error");
+};
+
+request.send();
+
+
+// This function is called from initMap
+function addATMs() {
+  
+  var request = new XMLHttpRequest();
+  request.open('GET', '', true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var result = JSON.parse(this.response);
+      console.log(result);
+      if (!result) {
+        alert("No ATMs returned");
+        return false;
+      }
+      drawpoints(result);
+    } else {
+      console.log("server returned an error");
+    }
+  };
+
+  request.onerror = function() {
+    console.log("connection error");
+  };
+
+  request.send();
+} // end addATMs
+
+
+// Draw the ATMs on the map
+function drawpoints(data) {
+
+  // Define an icon for our markers to overwrite the default
+  var icon = {
+    url: "https://assets.decoded.com/images/atm-machine-sign-hi.png",
+    scaledSize: new google.maps.Size(30, 30), // scaled size
+    origin: new google.maps.Point(0, 0), // origin
+    anchor: new google.maps.Point(0, 0), // anchor
+  }; // End icon
+
+  // Loop through our list o fATMs and put them on the map
+  for (i = 0; i < data.length; i++) {
+
+    // You have to create a point first
+    var point = new google.maps.LatLng(
+      data[i].Location.Point.Latitude, data[i].Location.Point.Longitude
+    );
+
+    // And then add a marker to the point
+    var marker = new google.maps.Marker({
+      position: point,
+      map: map,
+      icon: icon
+    });
+  }
+}
